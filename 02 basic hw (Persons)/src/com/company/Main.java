5e5c15d7a03e69142cccea89d381a793424ce382
Alex <a.a.kovalev@bk.ru>
2021-04-07 20:12:38 +0300

    Добавил класс Person и методы divorce, married

--- 02 basic hw (Persons)/src/com/company/Main.java
+++ 02 basic hw (Persons)/src/com/company/Main.java
@@ -8,10 +8,39 @@ public class Main {
     public static void main(String[] args) {
 
         Person Alex = new Person(true, "Alex");
+        Person Eduard = new Person(true, "Eduard");
+        Person Ivan = new Person(true, "Ivan");
 
-        System.out.println(Alex.getName());
+        Person Maria = new Person(false, "Maria");
+        Person Ann = new Person(false, "Ann");
+        Person Kate = new Person(false, "Kate");
 
 
+
+        if (Alex.marry(Maria))
+            System.out.println(Alex.getName() + " has married with " + Maria.getName());
+        else
+            System.out.println(Alex.getName() + " and "  + Maria.getName() + " have same gender");
+
+        if (Ann.marry(Eduard))
+            System.out.println(Ann.getName() + " has married with " + Eduard.getName());
+        else
+            System.out.println(Ann.getName() + " and "  + Eduard.getName() + " have same gender");
+
+        if (Ann.marry(Alex))
+            System.out.println(Ann.getName() + " has married with " + Alex.getName());
+        else
+            System.out.println(Ann.getName() + " and "  + Alex.getName() + " have same gender");
+
+        if (Ivan.marry(Eduard))
+            System.out.println(Ivan.getName() + " has married with " + Eduard.getName());
+        else
+            System.out.println(Ivan.getName() + " and "  + Eduard.getName() + " have same gender");
+
+        if (Ivan.marry(Kate))
+            System.out.println(Ivan.getName() + " has married with " + Kate.getName());
+        else
+            System.out.println(Ivan.getName() + " and "  + Kate.getName() + " have same gender");
 
     }
 }
--- 02 basic hw (Persons)/src/com/company/Person.java
+++ 02 basic hw (Persons)/src/com/company/Person.java
@@ -6,6 +6,49 @@ public class Person {
         this.name = name;
     }
 
+    /**
+     * This method checks gender of persons. If genders are not equal - tries to marry.
+     * If one of them has another spouse - execute divorce(sets spouse = null for husband and wife. Example: if both persons have spouses - then divorce will set 4 spouse to null) and then executes marry().
+     *
+     * @param person - new husband/wife for this person.
+     * @return - returns true if this person has another gender than passed person and they are not husband and wife, false otherwise
+     */
+    public boolean marry(Person person) {
+        if (person == null) {
+            return false;
+        }
+
+        if (man != person.man) {
+            if (spouse != null) {
+                this.divorce();
+            }
+            if (person.spouse != null) {
+                this.divorce();
+            }
+
+            spouse = person;
+            person.spouse = this;
+
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Sets spouse = null if spouse is not null
+     *
+     * @return true - if person status has been changed
+     */
+    public boolean divorce() {
+        boolean isNotNull = (spouse != null);
+        if (isNotNull) {
+            spouse.spouse = null;
+            spouse = null;
+        }
+
+        return isNotNull;
+    }
+
     public Person getSpouse() {
         return spouse;
     }
@@ -17,6 +60,7 @@ public class Person {
     public boolean getMan() {
         return man;
     }
+
 
     private final boolean man;
     private final String name;
