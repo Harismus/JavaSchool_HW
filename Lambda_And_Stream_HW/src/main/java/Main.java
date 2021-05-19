8a94c441d23761bcb395ff55d2212d0135ffa9aa
Alex <a.a.kovalev@bk.ru>
2021-05-19 18:59:10 +0300

    Убрал лишний код

--- Lambda_And_Stream_HW/src/main/java/Main.java
+++ Lambda_And_Stream_HW/src/main/java/Main.java
@@ -1,17 +1,8 @@
 import java.util.*;
-import java.util.concurrent.Callable;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
-import java.util.function.UnaryOperator;
 
 
 public class Main {
-
-
-
-
     public static void main(String[] args) {
-
 
         List<Person> someCollection = new ArrayList<>();
 
@@ -27,7 +18,6 @@ public class Main {
                 .filter(p -> p.getAge() > 20)
                 .transform( p -> new Person(p.getName(),p.getAge() + 30))
                 .toMap(p -> p.getName(), p -> p);
-
 
 
         m.forEach( (k, p) -> System.out.println(p.toString() +  " Name = " + p.getName() + " Age = " + p.getAge() ) );
