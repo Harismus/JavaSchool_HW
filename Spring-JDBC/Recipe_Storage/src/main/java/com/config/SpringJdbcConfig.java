6a953f5944dbb452e6612b0be21d49353909cce3
Alex <a.a.kovalev@bk.ru>
2021-06-17 17:34:10 +0300

    Подправил проект

--- Spring-JDBC/Recipe_Storage/src/main/java/com/config/SpringJdbcConfig.java
+++ Spring-JDBC/Recipe_Storage/src/main/java/com/config/SpringJdbcConfig.java
@@ -16,9 +16,9 @@ public class SpringJdbcConfig {
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setUrl("jdbc:h2:tcp://localhost/~/test");
-        dataSource.setUsername("user");
-        dataSource.setPassword("123");
+        dataSource.setUrl("jdbc:h2:~/test");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
 
         return dataSource;
     }
